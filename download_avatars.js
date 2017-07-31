768a59e3565993c8a38da388629e04a75a20da99
Alex Hache <ahache8@gmail.com>
2017-07-31 23:49:56 +0000

    Created request url string

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "ahache";
+var GITHUB_TOKEN = "26a33da516f7404f2a2349eaccfedbc7abb795cc";
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  request.get("https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors");
+  var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  // console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
