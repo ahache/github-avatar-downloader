44ed099328b833a59e937339689ddcab5b2d991a
Alex Hache <ahache8@gmail.com>
2017-08-01 00:11:19 +0000

    Fixed URL object to use User Agent

--- download_avatars.js
+++ download_avatars.js
@@ -7,7 +7,15 @@ var GITHUB_TOKEN = "26a33da516f7404f2a2349eaccfedbc7abb795cc";
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  // console.log(requestURL);
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  }
+  request(options, function (err, response, body) {
+    console.log(body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
